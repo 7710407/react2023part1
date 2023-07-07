95b16aa8c203b01704b49d716fb85b20b41e74ea
Arsen Davtyan <Arsen.Davtyan@microchip.com>
2023-07-07 17:47:31 +0400

    Added outlet

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import Footer from './components/Footer';
 import Header from './components/Header'
 import Contact from './pages/Contact'
 import Home from './pages/Home';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Outlet} from 'react-router-dom'
 
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <>
+      <Header />
+      <Outlet />
+      <Footer />
+      </>
+    )
+  }
+
   return (
   <>
-    <Header />
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='contact-us' element={<Contact />} />
       </Routes>
     </BrowserRouter>
-    <Footer />
   </>
   );
 }
