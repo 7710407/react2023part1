4d82dc505604b32867b1d961482ea4c052690bbf
7710407 <7710407@gmail.com>
2023-07-08 01:07:54 +0400

    Implement BrowserRoutes with createBrowserRouter

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,7 +3,8 @@ import Footer from './Footer';
 import Header from './Header'
 import Contact from '../pages/Contact'
 import Home from '../pages/Home';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
 export default function Router() {
     const Layout = () => {
@@ -16,20 +17,38 @@ export default function Router() {
         )
     }
     
-    const BrowserRoutes = () => {
-        return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Layout />}>
-                        <Route path='/' element={<Home />} />
-                        <Route path='contact-us' element={<Contact />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        )
-    }
+    // const BrowserRoutes = () => {
+    //     return (
+    //         <BrowserRouter>
+    //             <Routes>
+    //                 <Route path='/' element={<Layout />}>
+    //                     <Route path='/' element={<Home />} />
+    //                     <Route path='contact-us' element={<Contact />} />
+    //                 </Route>
+    //             </Routes>
+    //         </BrowserRouter>
+    //     )
+    //}
+
+    const BrowserRoutes = createBrowserRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+
+                {
+                    path: "/",
+                    element: <Home />
+                },
+                {
+                    path: "/contact-us",
+                    element: <Contact />
+                }
+            ]
+        }
+    ])
 
     return (
-        <BrowserRoutes />
+        <RouterProvider router={BrowserRoutes} />
     );
 }
