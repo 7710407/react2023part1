54ffce9da996253979d0b8e5f3cf8084c6dfcd0c
Artur01011 <9362561@gmail.com>
2023-07-09 21:34:04 +0400

    Added contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,21 @@
+import { useState } from "react"
 function Contact() {
+
+    const [email, setEmail] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <>
             <h1>Contact Us</h1>
+            <form>
+                <label>Email</label>
+                <input type="text" id="email" name="email" value="" onChange={(e) => setEmail(e.target.value)} />
+                <label>Message</label>
+                <textarea id="message" name="message"></textarea>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
+            </form>
         </>
     )
 }
